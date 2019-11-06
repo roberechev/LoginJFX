2886089dd24f3fe89d004d527face417b5a0208a
roberechev <rober.eche.sa@gmail.com>
2019-11-06 16:55:50 +0100

    Carrusel de imagenes de fondo

--- src/application/controlador.java
+++ src/application/controlador.java
@@ -1,5 +1,90 @@
 package application;
 
-public class controlador {
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
+public class controlador {
+	@FXML
+	private Pane p1;
+
+	@FXML
+	private Pane p2;
+
+	@FXML
+	private Pane p3;
+
+	@FXML
+	private Pane p4;
+
+	public void initialize() {
+
+//		p1.setStyle("-fx-background-image: url('playa1.jpg')");
+//		p2.setStyle("-fx-background-image: url('playa2.jpg')");
+//		p3.setStyle("-fx-background-image: url('playa3.jpg')");
+//		p4.setStyle("-fx-background-image: url('playa4.jpg')");
+
+		backgroundAnimation();
+
+	}
+
+	private void backgroundAnimation() {
+
+		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), p4);
+		fadeTransition.setFromValue(1);
+		fadeTransition.setToValue(0);
+		fadeTransition.play();
+
+		fadeTransition.setOnFinished(event -> {
+
+			FadeTransition fadeTransition1 = new FadeTransition(Duration.seconds(3), p3);
+			fadeTransition1.setFromValue(1);
+			fadeTransition1.setToValue(0);
+			fadeTransition1.play();
+
+			fadeTransition1.setOnFinished(event1 -> {
+				FadeTransition fadeTransition2 = new FadeTransition(Duration.seconds(3), p2);
+				fadeTransition2.setFromValue(1);
+				fadeTransition2.setToValue(0);
+				fadeTransition2.play();
+
+				fadeTransition2.setOnFinished(event2 -> {
+
+					FadeTransition fadeTransition0 = new FadeTransition(Duration.seconds(3), p2);
+					fadeTransition0.setFromValue(0);
+					fadeTransition0.setToValue(1);
+					fadeTransition0.play();
+
+					fadeTransition0.setOnFinished(event3 -> {
+
+						FadeTransition fadeTransition11 = new FadeTransition(Duration.seconds(3), p3);
+
+						fadeTransition11.setFromValue(0);
+						fadeTransition11.setToValue(1);
+						fadeTransition11.play();
+
+						fadeTransition11.setOnFinished(event4 -> {
+
+							FadeTransition fadeTransition22 = new FadeTransition(Duration.seconds(3), p4);
+
+							fadeTransition22.setFromValue(0);
+							fadeTransition22.setToValue(1);
+							fadeTransition22.play();
+
+							fadeTransition22.setOnFinished(event5 -> {
+
+								backgroundAnimation();
+							});
+
+						});
+
+					});
+
+				});
+			});
+
+		});
+
+	}
 }
